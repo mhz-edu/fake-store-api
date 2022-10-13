802da45e760483e4ca228a7a849fb7835b55ee83
Aravind Muthuraman <aravind_mr@vhitechlab.com>
2022-10-13 10:16:53 +0530

    Aravind | Basic Node setup made

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 //initializes
-const mongoose = require('mongoose');
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const dotenv = require('dotenv');
-const dotenvExpand = require('dotenv-expand');
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const dotenv = require("dotenv");
+const dotenvExpand = require("dotenv-expand");
 const myEnv = dotenv.config();
 dotenvExpand.expand(myEnv);
 
@@ -15,43 +15,46 @@ const app = express();
 const port = process.env.PORT || 6400;
 
 //routes
-const productRoute = require('./routes/product');
-const homeRoute = require('./routes/home');
-const cartRoute = require('./routes/cart');
-const userRoute = require('./routes/user');
-const authRoute = require('./routes/auth');
+const productRoute = require("./routes/product");
+const homeRoute = require("./routes/home");
+const cartRoute = require("./routes/cart");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 //middleware
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //view engine
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.disable('view cache');
+app.disable("view cache");
 
-app.use('/', homeRoute);
-app.use('/products', productRoute);
-app.use('/carts', cartRoute);
-app.use('/users', userRoute);
-app.use('/auth', authRoute);
-
+app.use("/", homeRoute);
+app.use("/products", productRoute);
+app.use("/carts", cartRoute);
+app.use("/users", userRoute);
+app.use("/auth", authRoute);
+const dbUrl =
+  process.env.NODE_ENV === "prod"
+    ? process.env.DATABASE_URL
+    : process.env.DATABASE_URL_LOCAL;
 //mongoose
-mongoose.set('useFindAndModify', false);
-mongoose.set('useUnifiedTopology', true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useUnifiedTopology", true);
 mongoose
-	.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-	.then(() => {
-		app.listen(port, () => {
-			console.log('connect');
-		});
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+  .connect(dbUrl, { useNewUrlParser: true })
+  .then(() => {
+    app.listen(port, () => {
+      console.log("connect");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 module.exports = app;
